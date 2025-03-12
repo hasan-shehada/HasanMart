d9c32a30d605a9b4835e4c98f7a35173b178d525
hasan-shehada <hasansh7531@gmail.com>
2025-03-12 05:28:39 -0400

    Fix products fetching 4

--- server/server.js
+++ server/server.js
@@ -6,22 +6,33 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Enable CORS to allow cross-origin requests from the frontend
-app.use(cors({ origin: "https://hasanmart.pages.dev" || "*" }));
+app.use(
+  cors({
+    origin: "https://hasanmart.pages.dev",
+    methods: ["GET"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
-// Serve static files (like images)
-app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  "/public",
+  express.static(path.join(__dirname, "public"), {
+    setHeaders: (res, filePath) => {
+      const imageExtensions = [".jpg", ".jpeg", ".png", ".webp"];
+      if (imageExtensions.some((ext) => filePath.endsWith(ext))) {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+      }
+    },
+  })
+);
 
-// Import and use product routes
 const productRoutes = require("./routes/productRoutes");
 app.use("/api/products", productRoutes);
 
-// Root route to check if the server is running
 app.get("/", (req, res) => {
   res.send("Server is up and running!!!");
 });
 
-// Start the server and listen on the specified port
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
